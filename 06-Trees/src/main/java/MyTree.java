99e3452860cde95fdb2c85123cbd6ed4e350352e
ebr <clkebr2601@gmail.com>
2023-02-28 10:35:35 +0100

    trees

--- 06-Trees/src/main/java/MyTree.java
+++ 06-Trees/src/main/java/MyTree.java
@@ -11,7 +11,7 @@ public class MyTree {
     public MyTree(Node root) {
         this.root = root;
     }
-public void insert(int value){
+    public void insert(int value){
         Node newNode=new Node(value);
         if(root==null) {
             root=newNode;
@@ -52,7 +52,7 @@ public void insert(int value){
         System.out.print(root.value+", ");
         inOrderTraversal(root.rightChild);
     }
-// Post-Order traversal  Left-Right-Root
+    // Post-Order traversal  Left-Right-Root
     public void postOrderTraversal(Node root){
         if (root==null) return;
         postOrderTraversal(root.leftChild);
@@ -60,8 +60,8 @@ public void insert(int value){
         System.out.print(root.value+", ");
 
     }
-    // Assignment BreadthFirst Traversal
-public  void levelOrderTraversal(){
+    // BreadthFirst Traversal
+    public  void levelOrderTraversal(){
       if(root==null) return;
       Queue<Node> queue = new LinkedList<>();
       queue.add(root);
@@ -70,9 +70,10 @@ public  void levelOrderTraversal(){
           System.out.print(toVisit.value+", ");
           if(toVisit.leftChild!=null) queue.add(toVisit.leftChild);
           if(toVisit.rightChild!=null) queue.add(toVisit.rightChild);
-      }// end while
-}// end levelOrder
-// task-1 implement contains method
+      }
+    }
+
+    // task-1 implement contains method
     public boolean contains(int value){
         if (root==null) return false;
         Node current=root;
@@ -94,7 +95,7 @@ public  void levelOrderTraversal(){
         printLeaves(root.leftChild);
         printLeaves(root.rightChild);
     }
-// task -4 count leaves of a BST
+    // task -4 count leaves of a BST
     public int countLeaves(Node root){
       if (root==null) return 0;
       if (isLeaf(root)) return 1;
